19b877b444fc9f418bbddb0dd1a1d5359c893bab
qianjiachun <189964430@qq.com>
2024-01-15 10:20:29 +0800

    fix: 修复了头像和简介获取失败的bug

--- index.user.js
+++ index.user.js
@@ -2,7 +2,27 @@
 // ==UserScript==
 // @name         Bilibili弹幕查询发送者
 // @namespace    https://github.com/qianjiachun
-// @version      2023.08.30.01
+// @version      2024.01.15.01
+// @icon         https://static.hdslb.com/mobile/img/512.png
+// @description  bilibili（b站/哔哩哔哩）根据弹幕查询发送者信息
+// @author       小淳
+// @match        *://www.bilibili.com/video/*
+// @match        *://www.bilibili.com/festival/*
+// @match        *://www.bilibili.com/bangumi/play/*
+// @match        *://www.bilibili.com/cheese/play/*
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @require      https://lib.baomitu.com/protobufjs/6.11.2/protobuf.min.js
+// @connect      bilibili.com
+// @run-at       document-start
+// @license      MIT
+// ==/UserScript==
+"use strict";
+// ==UserScript==
+// @name         Bilibili弹幕查询发送者
+// @namespace    https://github.com/qianjiachun
+// @version      2024.01.15.01
+// @icon         https://static.hdslb.com/mobile/img/512.png
 // @description  bilibili（b站/哔哩哔哩）根据弹幕查询发送者信息
 // @author       小淳
 // @match        *://www.bilibili.com/video/*
@@ -293,6 +313,7 @@ function renderSenderInfoWrap() {
     div.innerHTML = `
     <div class="senderinfo__close">X</div>
     <a title="点个Star吧~" href="https://github.com/qianjiachun/bilibili-danmaku-tracker" target="_blank" class="senderinfo__github"><svg t="1639304975096" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2323" width="16" height="16"><path d="M512 42.666667A464.64 464.64 0 0 0 42.666667 502.186667 460.373333 460.373333 0 0 0 363.52 938.666667c23.466667 4.266667 32-9.813333 32-22.186667v-78.08c-130.56 27.733333-158.293333-61.44-158.293333-61.44a122.026667 122.026667 0 0 0-52.053334-67.413333c-42.666667-28.16 3.413333-27.733333 3.413334-27.733334a98.56 98.56 0 0 1 71.68 47.36 101.12 101.12 0 0 0 136.533333 37.973334 99.413333 99.413333 0 0 1 29.866667-61.44c-104.106667-11.52-213.333333-50.773333-213.333334-226.986667a177.066667 177.066667 0 0 1 47.36-124.16 161.28 161.28 0 0 1 4.693334-121.173333s39.68-12.373333 128 46.933333a455.68 455.68 0 0 1 234.666666 0c89.6-59.306667 128-46.933333 128-46.933333a161.28 161.28 0 0 1 4.693334 121.173333A177.066667 177.066667 0 0 1 810.666667 477.866667c0 176.64-110.08 215.466667-213.333334 226.986666a106.666667 106.666667 0 0 1 32 85.333334v125.866666c0 14.933333 8.533333 26.88 32 22.186667A460.8 460.8 0 0 0 981.333333 502.186667 464.64 464.64 0 0 0 512 42.666667" p-id="2324"></path></svg></a>
+    <div style="display:flex;justify-content:center;">请先左键选中弹幕再右键查询</div>
     <div class="senderinfo__content">
         <div class="senderinfo__loading">
             <div class="senderinfo__card">
@@ -358,9 +379,12 @@ function renderSenderInfoCard(uidList) {
             onload: function(response) {
                 domLoading.style.display = "none";
                 let ret = response.response;
+                let parser = new DOMParser();
+                let doc = parser.parseFromString(ret, "text/html");
+                if (!doc) return;
                 let name = String(getStrMiddle(ret, `<title data-vue-meta="true">`, "的个人空间"));
-                let head = String(getStrMiddle(ret, `<div class="face"><img src="`, `"`));
-                let sign = String(getStrMiddle(ret, `<div class="desc"><span class="content">`, "</span>"));
+                let head = String(doc.querySelector(".m-space-info").querySelector(".face").querySelector("img").src);
+                let sign = String(doc.querySelector(".desc").querySelector(".content").innerHTML);
                 if (!name || name === "" || name === "false") return;
                 let html = `
                     <div class="senderinfo__card">
